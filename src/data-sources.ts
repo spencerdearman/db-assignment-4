4f352c288c0d7c7f71ec717e0eaed2a05b49c3d2
Spencer Dearman <dearmanspencer@gmail.com>
2025-11-13 12:56:57 -0600

    updating

--- src/data-sources.ts
+++ src/data-sources.ts
@@ -5,21 +5,21 @@ import * as AnalyticsModels from './models/analytics.models';
 
 /* sql datasource */
 export const mysqlDataSource = new DataSource({
-    type: 'mysql',
-    host: 'localhost',
-    port: 3306,
-    username: 'root',
-    password: 'sockmonkey',
-    database: 'sakila',
-    entities: Object.values(SourceModels),
-    logging: false,
+  type: 'mysql',
+  host: 'localhost',
+  port: 3306,
+  username: 'root',
+  password: 'sockmonkey',
+  database: 'sakila',
+  entities: Object.values(SourceModels),
+  logging: false,
 });
 
 /* sqlite datasource */
 export const sqliteDataSource = new DataSource({
-    type: 'sqlite',
-    database: 'analytics.db',
-    entities: Object.values(AnalyticsModels),
-    synchronize: false,
-    logging: false,
+  type: 'sqlite',
+  database: 'analytics.db',
+  entities: Object.values(AnalyticsModels),
+  synchronize: false,
+  logging: false,
 });
